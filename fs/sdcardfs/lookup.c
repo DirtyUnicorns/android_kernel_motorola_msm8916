768e2b30ee75c12d95287938f6296baa21e7353f
Daniel Rosenberg <drosen@google.com>
2017-03-09 22:11:08 -0800

    ANDROID: sdcardfs: use d_splice_alias
    adapted from wrapfs
    commit 9671770ff8b9 ("Wrapfs: use d_splice_alias")
    
    Refactor interpose code to allow lookup to use d_splice_alias.
    
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 35766959
    Change-Id: Icf51db8658202c48456724275b03dc77f73f585b

--- fs/sdcardfs/lookup.c
+++ fs/sdcardfs/lookup.c
@@ -243,28 +243,6 @@ static int sdcardfs_name_match(void *__buf, const char *name, int namelen,
 	return 0;
 }
 
-struct sdcardfs_name_data {
-	struct dir_context ctx;
-	const struct qstr *to_find;
-	char *name;
-	bool found;
-};
-
-static int sdcardfs_name_match(void *__buf, const char *name, int namelen,
-		loff_t offset, u64 ino, unsigned int d_type)
-{
-	struct sdcardfs_name_data *buf = (struct sdcardfs_name_data *) __buf;
-	struct qstr candidate = QSTR_INIT(name, namelen);
-
-	if (qstr_case_eq(buf->to_find, &candidate)) {
-		memcpy(buf->name, name, namelen);
-		buf->name[namelen] = 0;
-		buf->found = true;
-		return 1;
-	}
-	return 0;
-}
-
 /*
  * Main driver function for sdcardfs's lookup.
  *
@@ -281,6 +259,7 @@ static struct dentry *__sdcardfs_lookup(struct dentry *dentry,
 	const struct qstr *name;
 	struct path lower_path;
 	struct qstr dname;
+	struct dentry *ret_dentry = NULL;
 	struct sdcardfs_sb_info *sbi;
 
 	sbi = SDCARDFS_SB(dentry->d_sb);
