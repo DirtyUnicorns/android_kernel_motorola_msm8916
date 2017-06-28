54834fd4dd66997a03f97d567ff16d8176895247
Nathan Chancellor <natechancellor@gmail.com>
2017-06-27 18:22:13 -0700

    msm: mdss: Optimize state_notifier entry
    Inspired by https://github.com/neobuddy89/shamu/commit/d5927558dad6f54f02d2a8518bdb6a60d41a2384
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1280,6 +1280,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc |= mdss_dsi_unblank(pdata);
 		break;
 	case MDSS_EVENT_PANEL_ON:
+#ifdef CONFIG_STATE_NOTIFIER
+		state_resume();
+#endif
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
 		if (ctrl_pdata->bl_on_defer)
 			ctrl_pdata->bl_on_defer(ctrl_pdata);
